908e2fade110660dac285b8e80160fbc5e948a50
salmonkarp <garyaxelmuliyono@gmail.com>
2024-03-25 14:37:45 +0700

    Initialisation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+const api = {
+  key: "7a55633dfd01cca1c4daa732947515b3",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+  const search = evt => {
+    if (evt.key === "Enter"){
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      .then(res => res.json())
+      .then(result => {
+        setWeather(result);
+        setQuery('');
+      })
+    }
+  }
+
+  let date = String(new window.Date())
+  date = date.slice(3,15);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={
+      (typeof weather.main != "undefined")
+      ? ((weather.main.temp > 16)
+        ? 'App warm'
+        : 'App')
+      : 'App'
+    }>
+      <main>
+        <div className="search-box">
+          <input 
+          type="text"
+          className="search-bar"
+          placeholder="Search..."
+          onChange={e => setQuery(e.target.value)}
+          value={query}
+          onKeyPress={search}
+          />
+        </div>
+        {(typeof weather.main != "undefined") ? (
+        <div className="content">
+          <div className="location-box">
+            <div className="location">{weather.name}, {weather.sys.country}</div>
+            <div className="date">{date}</div>
+          </div>
+          <div className="weather-box">
+            <div className="temperature">{Math.round(weather.main.temp)}°C</div>
+          </div>
+          <div className="weather">{weather.weather[0].main}</div>
+        </div>
+        ) : (
+          <div className="content">
+            <h3 className='notFound'>No location found with given name.</h3>
+          </div>
+        )}
+      </main>
     </div>
   );
 }
